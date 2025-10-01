f9c8150b29fea4382d7aea57b18d6624f5b7b95a
Ifeanyi Ezeribe <fgstudeo@gmail.com>
2025-10-01 01:09:34 -0400

    first version

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Bigslick", layout="wide")
+
+st.image("images/logo.jpg", width=200)   # logo placeholder
+st.title("Welcome to Bigslick")
+st.write("This is the first version of our site, powered by Streamlit on Heroku.")
